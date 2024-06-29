177b5f3bf6f6e2f684b4b3af0f197f70db15c3f9
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-06-30 01:42:05 +0200

    build: use prettier 3

--- tools/commitlint/commitlint.config.ts
+++ tools/commitlint/commitlint.config.ts
@@ -2,4 +2,4 @@ import { type UserConfig } from '@commitlint/types';
 
 export default {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
-} as UserConfig;
+} satisfies UserConfig;
