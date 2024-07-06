73da2a7f8651e2ac847c2f9f37e294a1c535cb95
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-07-06 20:04:43 +0200

    chore: add shared config

--- packages/web-test-runner/jest.config.ts
+++ packages/web-test-runner/jest.config.ts
@@ -1,28 +1,4 @@
-import { Config } from 'jest';
-import { join, relative } from 'node:path';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { jestConfig } from '../../libs/util/src/index';
 
-const projectName = process.env.NX_TASK_TARGET_PROJECT ?? '';
-const workspaceRoot = process.env.NX_WORKSPACE_ROOT ?? '';
-const absoluteProjectRoot = process.cwd();
-const projectRoot = relative(workspaceRoot, absoluteProjectRoot);
-
-/* eslint-disable */
-export default projectName
-  ? ({
-      displayName: projectName,
-      preset: relative(
-        absoluteProjectRoot,
-        join(workspaceRoot, 'jest.preset.js'),
-      ),
-      transform: {
-        '^.+\\.[tj]s$': [
-          'ts-jest',
-          {
-            tsconfig: join(workspaceRoot, projectRoot, 'tsconfig.spec.json'),
-          },
-        ],
-      },
-      moduleFileExtensions: ['ts', 'js'],
-      coverageDirectory: join(workspaceRoot, 'coverage', projectRoot),
-    } satisfies Config)
-  : {};
+export default jestConfig();
