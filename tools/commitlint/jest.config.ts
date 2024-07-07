3437be72da3abd4af62276037c491da5c8f5b293
Robby Rabbitman <robby.rabbitman+github@gmail.com>
2024-07-07 19:25:56 +0200

    refactor(tools-commitlint): use jest for testing (#33)
    Co-authored-by: robby rabbitman <robby.rabbitman+nx-plus@gmail.com>

---
+++ tools/commitlint/jest.config.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { jestConfig } from '../../libs/util/src/index';
+
+export default jestConfig();
---
+++ tools/commitlint/src/commitlint.config.spec.ts
@@ -0,0 +1,36 @@
+import { getPackageManagerCommand, workspaceRoot } from '@nx/devkit';
+import { execSync } from 'child_process';
+
+describe('commitlint config', () => {
+  const runCommitlint = (commitMessage: string) =>
+    execSync(
+      `${getPackageManagerCommand().exec} nx run tools-commitlint:exec:message --value '${commitMessage}'`,
+      {
+        cwd: workspaceRoot,
+        stdio: 'ignore',
+      },
+    );
+
+  const commitMessage_expectToPass = {
+    'not_a_valid_type: add documentation': false,
+    'docs(not-a-valid-scope): add documentation': false,
+    'docs(tools-commitlint): add documentation': true,
+    'docs: add documentation': true,
+  };
+
+  for (const [commitMessage, expectToPass] of Object.entries(
+    commitMessage_expectToPass,
+  )) {
+    it(commitMessage, () => {
+      let actualPass = true;
+
+      try {
+        runCommitlint(commitMessage);
+      } catch {
+        actualPass = false;
+      }
+
+      expect(actualPass).toBe(expectToPass);
+    });
+  }
+});
