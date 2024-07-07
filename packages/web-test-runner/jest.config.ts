5e74eebd373ad94c1e68088b595040b2499c254b
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-07-07 19:49:31 +0200

    chore: wip

--- packages/web-test-runner/jest.config.ts
+++ packages/web-test-runner/jest.config.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { jestConfig } from '../../libs/util/src/index';
+import { jestConfig } from '../../libs/jest-util/src/index';
 
 export default jestConfig();
