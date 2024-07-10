dc8e3f5444ac73c68f7e1a090350f0afa09d2f55
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-07-10 22:38:04 +0200

    chore: wip

--- tools/vite/src/local-registry.setup.ts
+++ tools/vite/src/local-registry.setup.ts
@@ -1,9 +1,8 @@
 import { workspaceRoot } from '@nx/devkit';
 import {
   localRegistryTarget,
-  publishTarget,
+  publish,
 } from '@robby-rabbitman/nx-plus-tools-local-registry';
-import { spawn } from 'child_process';
 import { readCachedProjectConfiguration } from 'nx/src/project-graph/project-graph';
 import { join } from 'path';
 
@@ -13,46 +12,57 @@ export async function setup() {
 
   const project = readCachedProjectConfiguration(projectName);
 
-  // const { stopLocalRegistry } = await publish({
-  //   clearStorage: true,
-  //   stopLocalRegistry: false,
-  //   tag: 'local',
-  //   specifier: '0.0.0-local',
-  //   storage: join(
+  const { stopLocalRegistry } = await publish({
+    clearStorage: true,
+    stopLocalRegistry: false,
+    tag: 'local',
+    specifier: '0.0.0-local',
+    storage: join(
+      workspaceRoot,
+      'dist',
+      'vite-local-registry-storage',
+      project.root,
+    ),
+    projects: [],
+    verbose,
+    localRegistryTarget,
+  });
+
+  global.stopLocalRegistry = stopLocalRegistry;
+
+  // const args = [
+  //   'exec',
+  //   'nx',
+  //   'run',
+  //   publishTarget,
+  //   `--tag=local`,
+  //   `--specifier=0.0.0-local`,
+  //   `--storage='${join(
   //     workspaceRoot,
   //     'dist',
   //     'vite-local-registry-storage',
   //     project.root,
-  //   ),
-  //   projects: [],
-  //   verbose,
-  //   localRegistryTarget,
+  //   )}'`,
+  //   `--stopLocalRegistry=false`,
+  //   `--clearStorage=true`,
+  //   `--verbose=${verbose}`,
+  // ];
+
+  // const localRegistry = spawn('pnpm', args, {
+  //   cwd: workspaceRoot,
+  //   stdio: 'inherit',
+  //   env: process.env,
   // });
 
-  const localRegistry = spawn(
-    `pnpm exec nx run ${publishTarget}`,
-    [
-      `--tag=local`,
-      `--specifier=0.0.0-local`,
-      `--storage=${join(
-        workspaceRoot,
-        'dist',
-        'vite-local-registry-storage',
-        project.root,
-      )}`,
-      `--stopLocalRegistry=false`,
-      `--clearStorage=true`,
-      `--localRegistryTarget=${localRegistryTarget}`,
-      `--verbose=${verbose}`,
-    ],
-    {
-      cwd: workspaceRoot,
-      stdio: 'inherit',
-      env: process.env,
-    },
-  );
+  // localRegistry.on('error', (err) => {
+  //   console.error('Failed to start local registry process.', err);
+  // });
 
-  global.stopLocalRegistry = () => localRegistry.kill();
+  // localRegistry.on('close', (code) => {
+  //   console.log(`Local registry process exited with code ${code}`);
+  // });
+
+  // global.stopLocalRegistry = () => localRegistry.kill();
 }
 
 export function teardown() {
