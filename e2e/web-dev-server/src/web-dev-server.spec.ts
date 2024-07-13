5c5da59a7998128de3c840470816937085a96d75
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-07-13 22:42:43 +0200

    chore: wip

--- e2e/web-dev-server/src/web-dev-server.spec.ts
+++ e2e/web-dev-server/src/web-dev-server.spec.ts
@@ -1,14 +1,14 @@
 import { NxJsonConfiguration, ProjectConfiguration } from '@nx/devkit';
 import { readJson } from '@nx/plugin/testing';
 import { createE2eWorkspace } from '@robby-rabbitman/nx-plus-libs-e2e-util';
+import { execUntil } from '@robby-rabbitman/nx-plus-libs-node-util';
 import { execSync } from 'node:child_process';
-import { writeFileSync } from 'node:fs';
+import { rmSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 import { createVersionPermutations } from './util';
 
 const versionMatrix = createVersionPermutations({
-  // nxVersions: ['^17', '^18', '^19'],
-  nxVersions: ['^19'],
+  nxVersions: ['^17', '^18', '^19'],
   e2eProject: '@robby-rabbitman/nx-plus-web-dev-server@local',
   peerDependencies: {
     '@web/dev-server': ['^0.4.6'],
@@ -71,7 +71,7 @@ for (const { nxVersion, e2eProject, peerDependencies } of versionMatrix) {
       const config = {};
 
       writeFileSync(
-        join(workspaceRoot, 'some-js-project/web-dev-server.config.js'),
+        join(workspaceRoot, 'some-js-project/web-dev-server.config.mjs'),
         `export default ${JSON.stringify(config, null, 2)};`,
       );
 
@@ -89,11 +89,31 @@ for (const { nxVersion, e2eProject, peerDependencies } of versionMatrix) {
         executor: 'nx:run-commands',
         options: {
           command: 'web-dev-server',
-          config: 'web-dev-server.config.js',
+          config: 'web-dev-server.config.mjs',
           cwd: 'some-js-project',
           watch: true,
         },
       });
+    });
+
+    it('should run the Web Dev Server', async () => {
+      rmSync(join(workspaceRoot, 'some-js-project/src'), {
+        recursive: true,
+        force: true,
+      });
+
+      writeFileSync(
+        join(workspaceRoot, 'some-js-project/index.html'),
+        'hi, not valid html but w/e O_O',
+      );
+
+      await execUntil(
+        'nx run some-js-project:serve',
+        (log) => log.includes('Web Dev Server started...'),
+        {
+          cwd: workspaceRoot,
+        },
+      );
     });
   });
 }
