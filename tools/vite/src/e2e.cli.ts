7ab4e6fd593bb65cf30467b5feb1bf6f8ddb8ee7
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-07-14 04:37:55 +0200

    chore: wip

--- tools/vite/src/e2e.cli.ts
+++ tools/vite/src/e2e.cli.ts
@@ -1,36 +1,28 @@
 import { getPackageManagerCommand, workspaceRoot } from '@nx/devkit';
-import {
-  localRegistryTarget,
-  publish,
-} from '@robby-rabbitman/nx-plus-tools-local-registry';
-import { execSync } from 'child_process';
+import { exec, execSync } from 'child_process';
 import { join } from 'path';
 
 async function main() {
-  const verbose = process.env['NX_VERBOSE_LOGGING'] === 'true';
-
-  const { stopLocalRegistry } = await publish({
-    clearStorage: true,
-    stopLocalRegistry: false,
-    tag: 'local',
-    specifier: '0.0.0-local',
-    storage: join(workspaceRoot, 'dist', 'e2e-local-registry-storage'),
-    projects: [],
-    verbose,
-    localRegistryTarget,
-  });
+  const localRegistryTarget = 'tools-local-registry:serve';
+  const localRegistry = exec(
+    `pnpm exec nx run tools-local-registry --clearStorage true --stopLocalRegistry false --tag 'local' --specifier '0.0.0-local' --storage ${join(workspaceRoot, 'dist', 'e2e-local-registry-storage')} --localRegistryTarget ${localRegistryTarget}`,
+    { env: process.env },
+  );
 
   try {
     const packageManagerCommand = getPackageManagerCommand();
 
     execSync(
       `${packageManagerCommand.exec} nx affected -t e2e-version-matrix-vite`,
+      { env: process.env, stdio: 'inherit' },
     );
   } catch (error) {
     console.error(error);
     process.exit(1);
   } finally {
-    stopLocalRegistry();
+    if (!localRegistry.killed) {
+      localRegistry.kill();
+    }
   }
   process.exit();
 }
