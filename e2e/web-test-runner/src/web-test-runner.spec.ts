f3b7c016fc6f574eeeee38032f4e7651a381b0a5
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-07-14 13:42:22 +0200

    chore: wip

--- e2e/web-test-runner/src/web-test-runner.spec.ts
+++ e2e/web-test-runner/src/web-test-runner.spec.ts
@@ -1,81 +1,81 @@
 import { NxJsonConfiguration, ProjectConfiguration } from '@nx/devkit';
 import { readJson } from '@nx/plugin/testing';
-import { createE2eNxWorkspace } from '@robby-rabbitman/nx-plus-libs-e2e-util';
+import {
+  createE2eNxWorkspace,
+  installE2eProject,
+  readE2eProject,
+} from '@robby-rabbitman/nx-plus-libs-e2e-util';
 import { execSync } from 'node:child_process';
 import { join } from 'node:path';
 
-// TODO: keep in sync with package.json of 'web-test-runner'
-// reference it via env or nx devkit?
-const nxVersions = ['^17', '^18', '^19'];
+describe(`@robby-rabbitman/nx-plus-web-test-runner`, () => {
+  const { e2eNxWorkspaceName, e2ePackage } = readE2eProject({
+    peerDependencyEnvPrefix: 'E2E_PEER_DEPENDENCY_',
+  });
 
-for (const nxVersion of nxVersions) {
-  describe(`@robby-rabbitman/nx-plus-web-test-runner with nx@${nxVersion}`, () => {
-    let workspaceRoot = '';
+  if (!e2ePackage.peerDependencies['nx']) {
+    throw new Error('nx not in peer dependencies!');
+  }
 
-    beforeAll(() => {
-      workspaceRoot = createE2eNxWorkspace({
-        e2eProjectName: 'web-test-runner-e2e',
-        e2eNxWorkspaceName: `web-dev-server-e2e--${Date.now()}`,
-        e2eNxVersion: nxVersion,
-        createNxWorkspaceArgs: '--preset apps',
-      });
-    });
+  const workspaceRoot = createE2eNxWorkspace({
+    e2eProjectName: 'web-test-runner-e2e',
+    e2eNxWorkspaceName,
+    e2eNxVersion: e2ePackage.peerDependencies.nx,
+    createNxWorkspaceArgs: '--preset apps',
+  });
 
-    it('should install succesfully', () => {
-      execSync(`npm i -D @robby-rabbitman/nx-plus-web-test-runner@local`, {
-        cwd: workspaceRoot,
-      });
-      execSync('npm ls @robby-rabbitman/nx-plus-web-test-runner', {
-        cwd: workspaceRoot,
-      });
-    });
-
-    it("should add the plugin '@robby-rabbitman/nx-plus-web-test-runner/plugin'", () => {
-      execSync('nx generate @robby-rabbitman/nx-plus-web-test-runner:init', {
-        cwd: workspaceRoot,
-      });
-
-      const nxJson = readJson<NxJsonConfiguration>(
-        join(workspaceRoot, 'nx.json'),
-      );
-
-      expect(nxJson.plugins).toContainEqual({
-        plugin: '@robby-rabbitman/nx-plus-web-test-runner/plugin',
-        options: {
-          targetName: 'test',
-        },
-      });
-    });
-
-    it('should infer the Web Test Runner', () => {
-      execSync(
-        'nx generate @nx/js:library --name=some-js-project --linter=none --projectNameAndRootFormat=as-provided --unitTestRunner=none --no-interactive',
-        {
-          cwd: workspaceRoot,
-        },
-      );
-
-      execSync('touch some-js-project/web-test-runner.config.js', {
-        cwd: workspaceRoot,
-      });
-
-      const project = JSON.parse(
-        execSync('nx show project some-js-project --json', {
-          cwd: workspaceRoot,
-          encoding: 'utf-8',
-        }),
-      ) as ProjectConfiguration;
-
-      const testTarget = project.targets['test'];
-
-      expect(testTarget).toEqual({
-        configurations: {},
-        executor: 'nx:run-commands',
-        options: {
-          command:
-            'web-test-runner --config=some-js-project/web-test-runner.config.js',
-        },
-      });
+  it('should install succesfully', () => {
+    installE2eProject({
+      package: e2ePackage,
+      workspaceRoot,
     });
   });
-}
+
+  it("should add the plugin '@robby-rabbitman/nx-plus-web-test-runner/plugin'", () => {
+    execSync('nx generate @robby-rabbitman/nx-plus-web-test-runner:init', {
+      cwd: workspaceRoot,
+    });
+
+    const nxJson = readJson<NxJsonConfiguration>(
+      join(workspaceRoot, 'nx.json'),
+    );
+
+    expect(nxJson.plugins).toContainEqual({
+      plugin: '@robby-rabbitman/nx-plus-web-test-runner/plugin',
+      options: {
+        targetName: 'test',
+      },
+    });
+  });
+
+  it('should infer the Web Test Runner', () => {
+    execSync(
+      'nx generate @nx/js:library --name=some-js-project --linter=none --projectNameAndRootFormat=as-provided --unitTestRunner=none --no-interactive',
+      {
+        cwd: workspaceRoot,
+      },
+    );
+
+    execSync('touch some-js-project/web-test-runner.config.js', {
+      cwd: workspaceRoot,
+    });
+
+    const project = JSON.parse(
+      execSync('nx show project some-js-project --json', {
+        cwd: workspaceRoot,
+        encoding: 'utf-8',
+      }),
+    ) as ProjectConfiguration;
+
+    const testTarget = project.targets['test'];
+
+    expect(testTarget).toEqual({
+      configurations: {},
+      executor: 'nx:run-commands',
+      options: {
+        command:
+          'web-test-runner --config=some-js-project/web-test-runner.config.js',
+      },
+    });
+  });
+});
