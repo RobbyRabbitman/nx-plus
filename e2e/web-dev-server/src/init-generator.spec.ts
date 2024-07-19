1ad67637a74cb9c6b3599fe899ef3988d1a4e251
Robby Rabbitman <robby.rabbitman+github@gmail.com>
2024-07-19 21:17:38 +0200

    test(web-dev-server): add e2e for the init generator (#82)
    Co-authored-by: robby rabbitman <robby.rabbitman+nx-plus@gmail.com>

---
+++ e2e/web-dev-server/src/init-generator.spec.ts
@@ -0,0 +1,215 @@
+import {
+  getPackageManagerCommand,
+  NxJsonConfiguration,
+  writeJsonFile,
+} from '@nx/devkit';
+import { readJson } from '@nx/plugin/testing';
+import {
+  createE2eNxWorkspace,
+  installProject,
+  readE2eProject,
+} from '@robby-rabbitman/nx-plus-libs-e2e-util';
+import { execSync } from 'node:child_process';
+import { join } from 'node:path';
+import { WebDevServerInitGeneratorSchema } from 'packages/web-dev-server/src/generators/init';
+
+describe(`@robby-rabbitman/nx-plus-web-dev-server:init`, () => {
+  const npm = getPackageManagerCommand('npm');
+  let workspaceRoot: string;
+  let nxJsonSnapShotAfterCreateE2eNxWorkspace: NxJsonConfiguration;
+
+  beforeAll(() => {
+    const { e2eWorkspaceName, e2ePackage } = readE2eProject({
+      peerDependencyEnvPrefix: 'E2E_PEER_DEPENDENCY_',
+    });
+
+    if (!e2ePackage.peerDependencies['nx']) {
+      throw new Error('nx not in peer dependencies!');
+    }
+
+    workspaceRoot = createE2eNxWorkspace({
+      e2eProjectName: 'web-dev-server-e2e',
+      e2eNxWorkspaceName: `init${e2eWorkspaceName}`,
+      e2eNxVersion: e2ePackage.peerDependencies.nx,
+      createNxWorkspaceArgs: '--preset apps',
+    });
+
+    nxJsonSnapShotAfterCreateE2eNxWorkspace = readJson(
+      join(workspaceRoot, 'nx.json'),
+    );
+
+    installProject({
+      package: e2ePackage,
+      workspaceRoot,
+      packageManagerCommand: npm,
+    });
+  });
+
+  beforeEach(() => {
+    writeJsonFile(
+      join(workspaceRoot, 'nx.json'),
+      nxJsonSnapShotAfterCreateE2eNxWorkspace,
+    );
+  });
+
+  const runInitGenerator = ({
+    schema,
+  }: {
+    schema: WebDevServerInitGeneratorSchema;
+  }) => {
+    const args = Object.entries(schema)
+      .map(([name, value]) => `--${name}=${value}`)
+      .join(' ');
+
+    execSync(
+      `nx generate @robby-rabbitman/nx-plus-web-dev-server:init ${args}`,
+      {
+        cwd: workspaceRoot,
+      },
+    );
+
+    const nxJson = readJson<NxJsonConfiguration>(
+      join(workspaceRoot, 'nx.json'),
+    );
+
+    return { nxJson };
+  };
+
+  describe('should not modify the `nx.json` when the plugin is already registered in the `nx.json`', () => {
+    it('when provided as an string', async () => {
+      const before = structuredClone(nxJsonSnapShotAfterCreateE2eNxWorkspace);
+
+      before.plugins = ['@robby-rabbitman/nx-plus-web-dev-server/plugin'];
+
+      writeJsonFile(join(workspaceRoot, 'nx.json'), before);
+
+      const { nxJson } = runInitGenerator({ schema: {} });
+
+      expect(nxJson).toEqual(before);
+    });
+
+    it('when provided as an object', async () => {
+      const before = structuredClone(nxJsonSnapShotAfterCreateE2eNxWorkspace);
+
+      const serveTargetName = 'I am already registered :D';
+
+      before.plugins = [
+        {
+          plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+          options: {
+            serveTargetName,
+          } satisfies WebDevServerInitGeneratorSchema,
+        },
+      ];
+
+      writeJsonFile(join(workspaceRoot, 'nx.json'), before);
+
+      const { nxJson } = runInitGenerator({ schema: {} });
+
+      expect(nxJson).toEqual(before);
+    });
+  });
+
+  it('should register the plugin in the `nx.json` when the plugin is not registered in the `nx.json`', async () => {
+    const { nxJson } = runInitGenerator({ schema: {} });
+
+    expect(nxJson.plugins).toContainEqual(
+      expect.objectContaining({
+        plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+      }),
+    );
+  });
+
+  describe('schema', () => {
+    describe('serveTargetName', () => {
+      it('should use the provided value', () => {
+        const serveTargetName = 'web-dev-server';
+
+        const { nxJson } = runInitGenerator({ schema: { serveTargetName } });
+
+        expect(nxJson.plugins).toContainEqual(
+          expect.objectContaining({
+            plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+            options: {
+              serveTargetName,
+            } satisfies WebDevServerInitGeneratorSchema,
+          }),
+        );
+      });
+
+      it('should fall back to `serve` when the provided value is an empty string', () => {
+        const serveTargetName = '';
+
+        const { nxJson } = runInitGenerator({ schema: { serveTargetName } });
+
+        expect(nxJson.plugins).toContainEqual(
+          expect.objectContaining({
+            plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+            options: {
+              serveTargetName: 'serve',
+            } satisfies WebDevServerInitGeneratorSchema,
+          }),
+        );
+      });
+
+      it('should fall back to `serve` when the value is not provided', () => {
+        const { nxJson } = runInitGenerator({ schema: {} });
+
+        expect(nxJson.plugins).toContainEqual(
+          expect.objectContaining({
+            plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+            options: {
+              serveTargetName: 'serve',
+            } satisfies WebDevServerInitGeneratorSchema,
+          }),
+        );
+      });
+    });
+
+    describe.todo('skipFormatFiles');
+
+    describe('skipAddPlugin', () => {
+      it('should add the plugin to `nx.json` when the value is not provided', () => {
+        const { nxJson } = runInitGenerator({ schema: {} });
+
+        expect(nxJson.plugins).toContainEqual(
+          expect.objectContaining({
+            plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+          }),
+        );
+      });
+
+      it('should add the plugin to `nx.json` when the provided value is `false`', () => {
+        const { nxJson } = runInitGenerator({
+          schema: { skipAddPlugin: false },
+        });
+
+        expect(nxJson.plugins).toContainEqual(
+          expect.objectContaining({
+            plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+          }),
+        );
+      });
+
+      it('should not add the plugin to `nx.json` when the provided value is `true`', () => {
+        expect(
+          nxJsonSnapShotAfterCreateE2eNxWorkspace.plugins ?? [],
+        ).not.toContainEqual(
+          expect.objectContaining({
+            plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+          }),
+        );
+
+        const { nxJson } = runInitGenerator({
+          schema: { skipAddPlugin: true },
+        });
+
+        expect(nxJson.plugins ?? []).not.toContainEqual(
+          expect.objectContaining({
+            plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
+          }),
+        );
+      });
+    });
+  });
+});
--- e2e/web-dev-server/src/plugin.spec.ts
+++ e2e/web-dev-server/src/plugin.spec.ts
@@ -2,6 +2,7 @@ import {
   getPackageManagerCommand,
   NxJsonConfiguration,
   ProjectConfiguration,
+  writeJsonFile,
 } from '@nx/devkit';
 import { readJson } from '@nx/plugin/testing';
 import {
@@ -10,53 +11,42 @@ import {
   readE2eProject,
 } from '@robby-rabbitman/nx-plus-libs-e2e-util';
 import { execUntil } from '@robby-rabbitman/nx-plus-libs-node-util';
-import { WebDevServerTargetPluginSchema } from '@robby-rabbitman/nx-plus-web-dev-server/plugin';
 import { DevServerConfig } from '@web/dev-server';
 import { execSync } from 'node:child_process';
 import { rmSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
-describe(`@robby-rabbitman/nx-plus-web-dev-server`, () => {
+describe('@robby-rabbitman/nx-plus-web-dev-server/plugin', () => {
   const npm = getPackageManagerCommand('npm');
+  let workspaceRoot: string;
 
-  const { e2eWorkspaceName, e2ePackage } = readE2eProject({
-    peerDependencyEnvPrefix: 'E2E_PEER_DEPENDENCY_',
-  });
+  beforeAll(() => {
+    const { e2eWorkspaceName, e2ePackage } = readE2eProject({
+      peerDependencyEnvPrefix: 'E2E_PEER_DEPENDENCY_',
+    });
 
-  if (!e2ePackage.peerDependencies['nx']) {
-    throw new Error('nx not in peer dependencies!');
-  }
+    if (!e2ePackage.peerDependencies['nx']) {
+      throw new Error('nx not in peer dependencies!');
+    }
 
-  const workspaceRoot = createE2eNxWorkspace({
-    e2eProjectName: 'web-dev-server-e2e',
-    e2eNxWorkspaceName: e2eWorkspaceName,
-    e2eNxVersion: e2ePackage.peerDependencies.nx,
-    createNxWorkspaceArgs: '--preset apps',
-  });
+    workspaceRoot = createE2eNxWorkspace({
+      e2eProjectName: 'web-dev-server-e2e',
+      e2eNxWorkspaceName: `plugin${e2eWorkspaceName}`,
+      e2eNxVersion: e2ePackage.peerDependencies.nx,
+      createNxWorkspaceArgs: '--preset apps',
+    });
 
-  it('should install succesfully', () => {
     installProject({
       package: e2ePackage,
       workspaceRoot,
       packageManagerCommand: npm,
     });
-  });
 
-  it("should add the plugin '@robby-rabbitman/nx-plus-web-dev-server/plugin'", () => {
-    execSync('nx generate @robby-rabbitman/nx-plus-web-dev-server:init', {
-      cwd: workspaceRoot,
-    });
-
-    const nxJson = readJson<NxJsonConfiguration>(
-      join(workspaceRoot, 'nx.json'),
-    );
-
-    expect(nxJson.plugins).toContainEqual({
-      plugin: '@robby-rabbitman/nx-plus-web-dev-server/plugin',
-      options: {
-        serveTargetName: 'serve',
-      } satisfies WebDevServerTargetPluginSchema,
-    });
+    const nxJsonPath = join(workspaceRoot, 'nx.json');
+    const nxJson = readJson<NxJsonConfiguration>(nxJsonPath);
+    nxJson.plugins ??= [];
+    nxJson.plugins.push('@robby-rabbitman/nx-plus-web-dev-server/plugin');
+    writeJsonFile(nxJsonPath, nxJson);
   });
 
   it('should infer the Web Dev Server', () => {
