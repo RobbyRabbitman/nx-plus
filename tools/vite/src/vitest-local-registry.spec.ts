2913c0d23c3a1981e00fce990911e459bc52cc7c
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-07-19 22:13:38 +0200

    wip

--- tools/vite/src/vitest-local-registry.spec.ts
+++ tools/vite/src/vitest-local-registry.spec.ts
@@ -2,21 +2,14 @@ import { execSync } from 'child_process';
 
 describe('vitest local registry config', () => {
   it('should publish this workspace', () => {
-    const registrySearchResult = JSON.parse(
-      execSync(`pnpm search '@robby-rabbitman/nx-plus' --json`, {
-        encoding: 'utf-8',
-      }),
-    ) as { name: string }[];
+    const expectedToBePublished = [
+      '@robby-rabbitman/nx-plus-web-test-runner',
+      '@robby-rabbitman/nx-plus-web-dev-server',
+    ];
+    const registry = 'http://localhost:4321/';
 
-    expect(registrySearchResult).toMatchObject([
-      {
-        name: '@robby-rabbitman/nx-plus-web-test-runner',
-        version: '0.0.0-local',
-      },
-      {
-        name: '@robby-rabbitman/nx-plus-web-dev-server',
-        version: '0.0.0-local',
-      },
-    ]);
+    for (const expectedPackage of expectedToBePublished) {
+      execSync(`npm view ${expectedPackage} --registry ${registry}`);
+    }
   });
 });
