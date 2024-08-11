791636bcb39639653f60625ad5afafa3af5e3f11
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-08-11 20:31:15 +0200

    docs: describe tools

--- tools/commitlint/src/commitlint.config.ts
+++ tools/commitlint/src/commitlint.config.ts
@@ -1,5 +1,12 @@
-import { type UserConfig } from '@commitlint/types';
+import { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
+import { readCachedProjectGraph } from '@nx/devkit';
+
+const getNxProjects = () => Object.keys(readCachedProjectGraph().nodes);
 
 export default {
-  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  // extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': () => [RuleConfigSeverity.Error, 'always', getNxProjects()],
+  },
 } satisfies UserConfig;
