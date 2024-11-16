da346c4de5a99fa031b897f62442a976b51e8eb2
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-11-16 10:57:23 +0000

    build: devcontainer

---
+++ .devcontainer/post-create.bash
@@ -0,0 +1,12 @@
+echo "⚙️ Installing Node.js..."
+NODE_VERSION=$(jq -r .engines.node package.json)
+. ${NVM_DIR}/nvm.sh
+nvm install ${NODE_VERSION}
+nvm alias default ${NODE_VERSION}
+nvm use default
+
+echo "⚙️ Enabling corepack..."
+corepack enable
+
+echo "⚙️ Installing npm workspace dependencies..."
+pnpm install --frozen-lockfile
