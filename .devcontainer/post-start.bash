5eb82394059088e64a5f29bc3533186726460268
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-11-17 22:47:44 +0000

    wip

--- .devcontainer/post-start.bash
+++ .devcontainer/post-start.bash
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# TODO: in order to use a nx plugin locally, we need to build it first
+pnpm tsc --build tools/eslint-nx-dependency-checks-plugin/tsconfig.lib.json
+
 pnpm nx run tools-verdaccio:start
