400e2088ea489a6c36b2cfb8eb40b0d95e8f5e22
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-11-20 00:24:44 +0100

    wip

--- .devcontainer/exec-cmd.sh
+++ .devcontainer/exec-cmd.sh
@@ -3,11 +3,11 @@
 DEVCONTAINERS_CLI_VERSION=$(jq -r '.devDependencies["@devcontainers/cli"]' package.json)
 
 set -e
-pnpx @devcontainers/cli@$DEVCONTAINERS_CLI_VERSION up --log-level ${NX_PLUS_DEV_CONTAINER_LOG_LEVEL:-'info'} --workspace-folder . ${NX_PLUS_DEV_CONTAINER_UP_ARGS}
+pnpx @devcontainers/cli@$DEVCONTAINERS_CLI_VERSION up --log-level ${NX_PLUS_DEV_CONTAINER_LOG_LEVEL:-'info'} $NX_PLUS_DEV_CONTAINER_UP_ARGS --workspace-folder .
 
 set +e
 echo "⚙️ Executing command inside dev container:\n\n\t$@\n"
-pnpx @devcontainers/cli@$DEVCONTAINERS_CLI_VERSION exec --log-level ${NX_PLUS_DEV_CONTAINER_LOG_LEVEL:-'info'} --workspace-folder . ${NX_PLUS_DEV_CONTAINER_EXEC_ARGS} "$@"
+pnpx @devcontainers/cli@$DEVCONTAINERS_CLI_VERSION exec --log-level ${NX_PLUS_DEV_CONTAINER_LOG_LEVEL:-'info'} $NX_PLUS_DEV_CONTAINER_EXEC_ARGS --workspace-folder . "$@"
 CMD_EXIT_CODE=$?
 set -e
 
