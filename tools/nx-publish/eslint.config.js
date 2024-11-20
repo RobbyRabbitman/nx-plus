02b0bd1540e59982203ca77ce644da414243b57e
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-11-20 22:36:06 +0000

    wip

--- tools/nx-publish/eslint.config.js
+++ tools/nx-publish/eslint.config.js
@@ -1,4 +1,21 @@
 import { nodeEslint } from '@robby-rabbitman/nx-plus-tools-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [...nodeEslint.configs.all];
+export default [
+  ...nodeEslint.configs.all,
+  {
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          /**
+           * TODO: why is not found? find out how the rule resolves deps -
+           * apparently the nx graph also does find the ts file, does it use
+           * tsconfigs?
+           */
+          ignoredDependencies: ['@robby-rabbitman/nx-plus-node-util'],
+        },
+      ],
+    },
+  },
+];
