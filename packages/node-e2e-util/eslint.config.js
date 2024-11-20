f24257cd66faf940c91b57d00069d715d9c98b84
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-11-20 22:24:35 +0000

    wip

--- packages/node-e2e-util/eslint.config.js
+++ packages/node-e2e-util/eslint.config.js
@@ -1,17 +1,4 @@
 import { nodeEslint } from '@robby-rabbitman/nx-plus-tools-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...nodeEslint.configs.all,
-  {
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          /** TODO: why is not found? find out how the rule resolves deps */
-          ignoredDependencies: ['@robby-rabbitman/nx-plus-node-util'],
-        },
-      ],
-    },
-  },
-];
+export default [...nodeEslint.configs.all];
