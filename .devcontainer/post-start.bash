e767536a3577ab8b93529c2c9522a3296452770d
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-11-21 17:52:23 +0000

    wip

--- .devcontainer/post-start.bash
+++ .devcontainer/post-start.bash
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-# in order to use a nx plugin locally, we need to build it first
-echo "⚙️ Building tools-eslint-nx-dependency-checks-plugin ..."
-pnpm tsc --build tools/eslint-nx-dependency-checks-plugin/tsconfig.lib.json
-
-# in order for projects to use the vite configs, we need to build them first
-echo "⚙️ Building tools-vite ..."
-pnpm tsc --build tools/vite/tsconfig.lib.json
-
-# in order for projects to use the eslint configs, we need to build them first
-echo "⚙️ Building tools-eslint ..."
-pnpm tsc --build tools/eslint/tsconfig.lib.json
-
 pnpm nx run tools-verdaccio:start
