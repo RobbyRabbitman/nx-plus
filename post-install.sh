ce89b8adf2104c02d27f6be61fc6e41a027c072f
robby rabbitman <robby.rabbitman+nx-plus@gmail.com>
2024-11-23 11:37:52 +0000

    3

--- post-install.sh
+++ post-install.sh
@@ -2,7 +2,7 @@
 
 # in order to use a nx plugin locally, we need to build it first
 echo "⚙️ Building tools-eslint-nx-dependency-checks-plugin ..."
-pnpm tsc --build tools/eslint-nx-dependency-checks-plugin/tsconfig.lib.json
+pnpm tsc --build tools/eslint-nx-dependency-checks-plugin/tsconfig.lib-cjs.json
 
 # in order for projects to use the vite configs, we need to build them first
 echo "⚙️ Building tools-vite ..."
