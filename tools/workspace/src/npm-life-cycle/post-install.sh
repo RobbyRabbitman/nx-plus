1778901aa56477ab28417c7d23cc9e5fef879d44
robert hasemann <robert.hasemann@hdi.de>
2025-02-21 22:09:52 +0100

    .

---
+++ tools/workspace/src/npm-life-cycle/post-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# echo "⚙️ Building tools ..."
+# for tool in "tools"/*; do
+#   if [ -d "$tool" ]; then
+#     tsconfig="$tool/tsconfig.lib.json"
+
+#     if [ -f "$tsconfig" ]; then
+#       echo "⚙️ Building $tool ..."
+#       pnpm tsc --build $tsconfig
+#     fi
+#   fi
+# done
+
+# echo "⚙️ Starting verdaccio ..."
+# pnpm nx run tools-verdaccio:start
