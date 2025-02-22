bd087ac1d116cfc1c48c5c21805e9365ec0d7e6b
robert hasemann <robert.hasemann@hdi.de>
2025-02-22 12:36:06 +0100

    .

---
+++ tools/vitest/src/configs/node-typescript.ts
@@ -0,0 +1,29 @@
+import { logger } from '@nx/devkit';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
+
+export function nodeTypescript() {
+  const config = defineConfig({
+    test: {
+      globals: true,
+      environment: 'node',
+      reporters: ['default', 'hanging-process'],
+      coverage: {
+        enabled: true,
+        clean: true,
+        include: ['src/**/*'],
+        exclude: [...coverageConfigDefaults.exclude, '**/index.ts'],
+        thresholds: {
+          branches: 95,
+          functions: 95,
+          lines: 95,
+          statements: 95,
+        },
+        reporter: [...coverageConfigDefaults.reporter, 'lcov'],
+      },
+    },
+  });
+
+  logger.verbose('[nodeTypescript] vitest config', config);
+
+  return config;
+}
