f54547e66b58702ea363c541ee3a9f1848d1e609
robert hasemann <robert.hasemann@hdi.de>
2025-02-22 23:07:16 +0100

    .

--- examples/web-test-runner/eslint.config.js
+++ examples/web-test-runner/eslint.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { nxPlusEslintNodeConfig } from '@robby-rabbitman/nx-plus-tools-eslint';
+import { nxPlusEslintJsConfig } from '@robby-rabbitman/nx-plus-tools-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [...nxPlusEslintNodeConfig];
+export default [...nxPlusEslintJsConfig];
