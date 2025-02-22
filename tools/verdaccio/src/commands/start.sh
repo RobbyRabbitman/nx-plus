dcc3df3563075dece2a41e9f57b7db32751789c2
robert hasemann <robert.hasemann@hdi.de>
2025-02-22 23:44:50 +0100

    .

--- tools/verdaccio/src/commands/start.sh
+++ tools/verdaccio/src/commands/start.sh
@@ -3,4 +3,4 @@
 echo "⚙️ Starting verdaccio ..."
 
 mkdir -p tools/verdaccio/.logs
-nohup sh -c 'pnpm verdaccio --config tools/verdaccio/src/config/config.yaml > tools/verdaccio/.logs/verdaccio.log 2>&1 &' >/dev/null
+nohup sh -c 'pnpm verdaccio --config tools/verdaccio/src/config/config.yaml > tools/verdaccio/.logs/verdaccio.log 2>&1 &' > /dev/null
