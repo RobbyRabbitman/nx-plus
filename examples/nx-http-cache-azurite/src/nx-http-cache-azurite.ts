a35aa319263ad5890ae1a45bdbe22f7126058250
robby rabbitman <robby.rabbitman@gmail.com>
2025-04-18 22:10:29 +0200

    azurite

--- examples/nx-http-cache-azurite/src/nx-http-cache-azurite.ts
+++ examples/nx-http-cache-azurite/src/nx-http-cache-azurite.ts
@@ -5,10 +5,20 @@ import {
 import { nxHttpCacheHandlerForAzureBlobStorage } from '@robby-rabbitman/nx-plus-nx-http-cache-azure-blob-storage';
 import { createServer } from 'http';
 
+/**
+ * Configure nx by adding a `.env` file in the root of the workspace before
+ * running the server.
+ *
+ * ```sh
+ * NX_SELF_HOSTED_REMOTE_CACHE_SERVER=http://localhost:3000
+ * NX_SELF_HOSTED_REMOTE_CACHE_ACCESS_TOKEN=dummy-token
+ * ```
+ */
+
 const client = new BlobServiceClient(
   'https://127.0.0.1:10000/devstoreaccount1',
   /**
-   * TODO: `DefaultAzureCredential` is not supported by azurite.
+   * TODO: `new DefaultAzureCredential()` is not supported by azurite.
    *
    * https://github.com/Azure/Azurite/issues/2303#issuecomment-2392418810
    */
@@ -23,8 +33,9 @@ await client.getContainerClient('nx-cache').createIfNotExists();
 createServer(
   await nxHttpCacheHandlerForAzureBlobStorage(
     {
-      readAccessToken: '1',
-      writeAccessToken: '2',
+      readAccessToken: '123',
+      writeAccessToken: process.env
+        .NX_SELF_HOSTED_REMOTE_CACHE_ACCESS_TOKEN as string,
     },
     {
       container: 'nx-cache',
