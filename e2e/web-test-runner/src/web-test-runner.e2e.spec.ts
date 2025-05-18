13aa91d7644a070eaa19fe30cf558eb801ebdfab
robby rabbitman <robby.rabbitman@gmail.com>
2025-05-18 20:38:49 +0200

    ci: increate e2e timeouts

--- e2e/web-test-runner/src/web-test-runner.e2e.spec.ts
+++ e2e/web-test-runner/src/web-test-runner.e2e.spec.ts
@@ -39,7 +39,7 @@ describe('[e2e Test] Web Test Runner', () => {
   it(
     "should run the 'Web Test Runner'",
     {
-      timeout: 15_000,
+      timeout: 25_000,
     },
     () => {
       expect(() => invokeWebTestRunnerOfExampleProject()).not.toThrow();
