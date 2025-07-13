bb0e4625a287be06f7e20fbaf7a5eae3095e7a35
Robby Rabbitman <robby.rabbitman+github@gmail.com>
2025-07-13 12:30:25 +0200

    ci: disable nx cloud on max limit (#780)
    Co-authored-by: robby rabbitman <robby.rabbitman@gmail.com>

---
+++ .github/scripts/install-deps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set +e
+installLog=$(pnpm i 2>&1)
+exit_code=$?
+set -e
+
+echo "$installLog"
+
+NX_CLOUD_DISABLED_MSG="This Nx Cloud organization has been disabled due to exceeding the FREE plan."
+
+if [ $exit_code -ne 0 ] && echo "$installLog" | grep -qi "$NX_CLOUD_DISABLED_MSG"; then
+  echo "Nx Cloud organization has been disabled, disabling Nx Cloud for this run."
+  echo "NX_NO_CLOUD=true" >>"$GITHUB_ENV"
+  # trigger postinstall scripts again, because they might have been errored due to the Nx Cloud error
+  export NX_NO_CLOUD=true
+  pnpm i
+else
+  exit $exit_code
+fi
