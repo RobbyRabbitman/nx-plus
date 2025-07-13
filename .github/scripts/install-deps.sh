036be9b912335c37fb998c494f20169a4f73b3b2
robby rabbitman <robby.rabbitman@gmail.com>
2025-07-13 12:07:48 +0200

    ref

--- .github/scripts/install-deps.sh
+++ .github/scripts/install-deps.sh
@@ -9,7 +9,7 @@ echo "$installLog"
 
 NX_CLOUD_DISABLED_MSG="This Nx Cloud organization has been disabled due to exceeding the FREE plan."
 
-if echo "$installLog" | grep -qi "$NX_CLOUD_DISABLED_MSG"; then
+if [ $exit_code -ne 0 ] && echo "$installLog" | grep -qi "$NX_CLOUD_DISABLED_MSG"; then
   echo "Nx Cloud organization has been disabled, disabling Nx Cloud for this run."
   echo "NX_NO_CLOUD=true" >>"$GITHUB_ENV"
   # trigger postinstall scripts again, because they might have been errored due to the Nx Cloud error
